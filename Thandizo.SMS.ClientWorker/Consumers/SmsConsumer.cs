412c2e6c86d017da233d2859a7c745a3c99c15ed
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-04-09 15:00:36 +0200

    Added SmsConsumer

---
+++ Thandizo.SMS.ClientWorker/Consumers/SmsConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Thandizo.DataModels.Contracts;
+using Thandizo.Messaging.Core;
+
+namespace Thandizo.SMS.ClientWorker.Consumers
+{
+    public class SmsConsumer : IConsumer<IMessageModelRequest>
+    {
+        private readonly IMessagingService _service;
+
+        public SmsConsumer(IMessagingService service)
+        {
+            _service = service;
+        }
+        public async Task Consume(ConsumeContext<IMessageModelRequest> context)
+        {
+            var request = context.Message;
+            var response = await _service.SendMessage(request.Message);
+            await context.RespondAsync(response);
+        }
+    }
+}
