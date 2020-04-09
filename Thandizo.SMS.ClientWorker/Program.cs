efd4b7ba0b245cec14fffe4571285482d66c60e5
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-04-09 14:20:49 +0200

    Add project files.

---
+++ Thandizo.SMS.ClientWorker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Thandizo.SMS.ClientWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
